ab2e7f3cd2b3f13ab653696734e25b8883b6f7d8
shivarajkulal <shivarajkulalsn884@gmail.com>
2024-03-19 22:51:43 +0530

    Weather App

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 5000;
 app.get("/", (req, res) => {
   //const address = req.query.address; 
   //const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${API_KEY}`;
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=40.7128&lon=-74.0060&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=40.7128&lon=-74.0060&appid=${API_KEY}`;
   console.log(url);
 
   axios
